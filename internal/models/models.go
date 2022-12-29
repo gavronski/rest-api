de9ea7ca59a4db12a785e21402aeb5f60344ceea
gavronski <p.gawronski@ideo.pl>
2022-12-29 23:56:56 +0100

    add a player model

---
+++ internal/models/models.go
@@ -0,0 +1,13 @@
+package models 
+
+type Player struct {
+	ID int 
+	FirstName string 
+	LastName string 
+	Age int 
+	Country string 
+	Club string 
+	Position string 
+	Goals int 
+	Assists int 
+}
