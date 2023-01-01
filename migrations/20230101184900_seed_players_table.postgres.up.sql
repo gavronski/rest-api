ed80bbd3edf3bb7f614813d063f8460c41b17b64
gavronski <p.gawronski@ideo.pl>
2023-01-01 21:33:44 +0100

    seeders for players table migrations

---
+++ migrations/20230101184900_seed_players_table.postgres.up.sql
@@ -0,0 +1,5 @@
+INSERT INTO players (id, first_name, last_name, age, country, club, position, goals, assists, created_at, updated_at) VALUES
+	(1, 'Kylian', 'Mbappe', 24, 'France', 'Paris Saint-Germain', 'striker', 34, 15, '2022-12-11 12:29:54.395025', '2022-12-11 12:29:54.395025'),
+	(2, 'Lionel', 'Messi', 35, 'Argentina', 'Paris Saint-Germain', 'right striker', 23, 18, '2022-12-11 12:29:54.395025', '2022-12-11 12:29:54.395025'),
+	(3, 'Robert', 'Lewandowski', 34, 'Poland', 'FC Barcelona', 'striker', 41, 12, '2022-12-11 12:29:54.395025', '2022-12-11 12:29:54.395025');
+
