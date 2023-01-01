4f1c6a94dbb61cee8a6acf086e4b4365eb3b2ccb
gavronski <p.gawronski@ideo.pl>
2023-01-01 21:35:56 +0100

    get all players postgres method

--- internal/repository/dbrepo/postgres.go
+++ internal/repository/dbrepo/postgres.go
@@ -2,9 +2,46 @@ package dbrepo
 
 import (
 	"app/internal/models"
+	"context"
+	"log"
+	"time"
 )
 
 func (m *postgresDBRepo) GetPlayers() ([]models.Player, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
 	var players = []models.Player{}
+
+	query := `select * from players`
+	rows, err := m.DB.QueryContext(ctx, query)
+
+	if err != nil {
+		return players, err
+	}
+
+	for rows.Next() {
+		var player models.Player
+
+		err = rows.Scan(
+			&player.ID,
+			&player.FirstName,
+			&player.LastName,
+			&player.Age,
+			&player.Country,
+			&player.Club,
+			&player.Position,
+			&player.Goals,
+			&player.Assists,
+			&player.CreatedAt,
+			&player.UpdatedAt,
+		)
+
+		if err != nil {
+			return players, err
+		}
+		log.Println(player)
+		players = append(players, player)
+	}
 	return players, nil
 }
