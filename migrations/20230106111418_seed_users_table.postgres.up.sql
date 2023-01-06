daa058c56ebc548c6defb178b05b64ae5f32294f
gavronski <pawelgawronski08@gmail.com>
2023-01-06 12:17:43 +0100

    users table seeders

---
+++ migrations/20230106111418_seed_users_table.postgres.up.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (id, login, password, created_at, updated_at) VALUES
+	(1, 'admin', '$2a$12$aI2cJ823uqA4TB/atn8XWeNVDD8BzAZmx2rbC74dpVyPv6t4JIa5e', '2023-01-01 00:00:00', '2023-01-01 00:00:00');
